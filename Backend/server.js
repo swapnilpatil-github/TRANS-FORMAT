5048cd48da1df4c1fe355cbdccc48c7c245568e0
swapnilpatil-github <swapnilpatil1012003@gmail.com>
2024-08-13 20:31:38 +0530

    added dynamic port and updated packages.json

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ import authRoutes from './Routes/authRoutes.js';
 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Increase payload size limit
 app.use(bodyParser.json({ limit: '50mb' }));
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/image/convert': 'http://localhost:5000',
+      '/image/convert': `http://localhost:5000`,
       '/audio/convert': 'http://localhost:5000',
       '/video/convert': 'http://localhost:5000',
      
